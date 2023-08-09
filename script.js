3531a5bde6d8e93a89e333c9d66460e587877ace
Edmund Frio <edmundfrio@gmail.com>
2023-08-09 09:37:24 -1000

    added toggle button and functionality

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-const backgroundColor = document.querySelector("body")
-const colorButton = document.getElementById("color-btn")
+let backgroundColor = document.querySelector("body");
+let toggleBtn = document.querySelector(".toggle-btn");
 
-const hex = []
+function toggleDarkMode() {
+  backgroundColor.classList.toggle("dark-mode");
+  let currentBtn = toggleBtn.classList.toggle("dark-mode");
 
-colorButton.addEventListener("click", () => {
+  if (currentBtn !== true) {
+    toggleBtn.innerHTML = "Dark Mode";
+  } else {
+    toggleBtn.innerHTML = "Light Mode";
+  }
+}
 
-  })
+toggleBtn.addEventListener("click", () => {
+  let nextProject = document.getElementById("next-project");
+  let clicks = 0;
+  let a = document.createElement("a");
+
+  clicks++;
+
+  console.log(clicks);
+});
