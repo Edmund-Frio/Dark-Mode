4fa7bc1c86a3d8f011f8c92d8f37d6e97f9a7e59
Edmund-Frio <edmundfrio@gmail.com>
2023-08-09 16:48:50 -1000

    added btn random position functionality

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
 let backgroundColor = document.querySelector("body");
 let toggleBtn = document.querySelector(".toggle-btn");
+let githubLink = document.querySelector(".link");
 
 function toggleDarkMode() {
   backgroundColor.classList.toggle("dark-mode");
   let currentBtn = toggleBtn.classList.toggle("dark-mode");
 
-  if (currentBtn !== true) {
+  if (currentBtn === true) {
     toggleBtn.innerHTML = "Dark Mode";
+    githubLink.style.color = "#d4cec4";
   } else {
     toggleBtn.innerHTML = "Light Mode";
+    githubLink.style.color = "#090614";
   }
+
+  toggleBtn.addEventListener("click", () => {
+    let randY = Math.floor((Math.random() * 400));
+    let randX = Math.floor((Math.random() * 400));
+    backgroundColor.style.transform = `translate(${randX}px, ${randY}px)`;
+  
+    backgroundColor.appendChild(cloneDark);
+  })
+  
+
 }
-
-toggleBtn.addEventListener("click", () => {
-  let nextProject = document.getElementById("next-project");
-  let clicks = 0;
-  let a = document.createElement("a");
-
-  clicks++;
-
-  console.log(clicks);
-});
